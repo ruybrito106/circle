a9b06e1f232108b78826b4b1f29f9592425d2742
Victor Duarte Diniz Monteiro <victorddiniz@gmail.com>
2019-04-21 16:34:53 -0700

    add react-native-svg dependecy and link to app

--- android/app/src/main/java/com/circleapp/MainApplication.java
+++ android/app/src/main/java/com/circleapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.circleapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.reactcommunity.rnlocalize.RNLocalizePackage;
@@ -27,6 +28,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
             new RNGestureHandlerPackage(),
             new SplashScreenReactPackage(),
             new RNLocalizePackage(),

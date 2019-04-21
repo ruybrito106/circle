b42774b34756ecbe6e6a5b9aa1b75dd972cf723a
Victor Duarte Diniz Monteiro <victorddiniz@gmail.com>
2019-04-21 16:41:25 -0700

    add circle-logo component

---
+++ app/components/circle-logo/circle-logo.props.ts
@@ -0,0 +1,10 @@
+export interface CircleLogoProps {
+  /** Background color of outer circle */
+  outerCircleColor: string
+
+  /** Background color for inner circle */
+  innerCircleColor: string
+
+  /** Width and height of circle's view */
+  dimension: number
+}

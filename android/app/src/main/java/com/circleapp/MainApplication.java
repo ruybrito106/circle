6c81f5e666fc75bccca7121ff38e014418cf418d
Victor Duarte Diniz Monteiro <victorddiniz@gmail.com>
2019-06-15 01:33:23 -0700

    configura app for Google Sign-in

--- android/app/src/main/java/com/circleapp/MainApplication.java
+++ android/app/src/main/java/com/circleapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.circleapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
 import com.horcrux.svg.SvgPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGoogleSigninPackage(),
             new SvgPackage(),
             new RNGestureHandlerPackage(),
             new SplashScreenReactPackage(),

2e0bb676eec7109bac91830978c77d6d42cfb113
Victor Duarte Diniz Monteiro <victorddiniz@gmail.com>
2019-06-17 00:39:51 -0700

    add sign-in logic to splash-screen

--- app/screens/splash-screen/splash-screen.tsx
+++ app/screens/splash-screen/splash-screen.tsx
@@ -1,72 +1,97 @@
-import { observer } from "mobx-react"
+import { inject, observer } from "mobx-react"
 import * as React from "react"
-import { Animated, Easing, ViewStyle } from "react-native"
+import { View, ViewStyle } from "react-native"
+import { GoogleSignin, GoogleSigninButton, statusCodes } from "react-native-google-signin"
 import { NavigationScreenProps } from "react-navigation"
 import { CircleLogo } from "../../components/circle-logo"
 import { Screen } from "../../components/screen"
+import { CircleUserSnapshot } from "../../models/circle-user"
+import { RootStore } from "../../models/root-store"
+import { ScopesNeeded } from "../../services/google/sheets-api"
 import { color } from "../../theme"
 import { palette } from "../../theme/palette"
 
-export interface SplashScreenProps extends NavigationScreenProps<{}> {}
+export interface SplashScreenProps extends NavigationScreenProps<{}> {
+  rootStore?: RootStore
+}
 
 const ROOT: ViewStyle = {
   backgroundColor: color.palette.purpleBlue,
 }
 
-const CIRCLE_INITIAL_HEIGHT = 0.38
-const CIRCLE_FINAL_HEIGHT_PERCENTAGE_LOG_IN_PAGE = 0.07
-const CIRCLE_FINAL_HEIGHT_PERCENTAGE_HOME_PAGE = 0.2
-const ANIMATION_DEFAULT_SETTINGS = {
-  easing: Easing.in(Easing.quad),
-  duration: 1500,
-}
+const CIRCLE_INITIAL_HEIGHT = 0.3
+const SIGN_IN_BUTTON_HEIGHT = CIRCLE_INITIAL_HEIGHT + 0.05
 
-// @inject("mobxstuff")
 @observer
+@inject("rootStore")
 export class SplashScreen extends React.Component<SplashScreenProps, {}> {
   state = {
-    circleHeightAnim: new Animated.Value(CIRCLE_INITIAL_HEIGHT),
-    circleHeight: CIRCLE_INITIAL_HEIGHT,
-    isLoggedIn: false,
+    isSigninInProgress: false,
   }
 
-  private moveCircleToLogInScreenPosition() {
-    Animated.timing(this.state.circleHeightAnim, {
-      toValue: CIRCLE_FINAL_HEIGHT_PERCENTAGE_LOG_IN_PAGE,
-      ...ANIMATION_DEFAULT_SETTINGS,
-    }).start()
-  }
-
-  private moveCircleToHomeScreenPosition() {
-    Animated.timing(this.state.circleHeightAnim, {
-      toValue: CIRCLE_FINAL_HEIGHT_PERCENTAGE_HOME_PAGE,
-      ...ANIMATION_DEFAULT_SETTINGS,
-    }).start()
+  private get store(): RootStore {
+    return this.props.rootStore!
   }
 
   componentDidMount() {
-    this.state.circleHeightAnim.addListener(currentValue =>
-      this.setState({ circleHeight: currentValue.value }),
-    )
-    if (this.state.isLoggedIn) {
-      this.moveCircleToHomeScreenPosition()
-    } else {
-      this.moveCircleToLogInScreenPosition()
+    if (!this.store.circleUser) {
+      GoogleSignin.configure({
+        scopes: ScopesNeeded,
+      })
     }
   }
 
   render() {
-    const circleHeightInPercentage = `${this.state.circleHeight * 100}%`
+    const circleHeight = `${CIRCLE_INITIAL_HEIGHT * 100}%`
+    const buttonHeight = `${SIGN_IN_BUTTON_HEIGHT * 100}%`
 
     return (
       <Screen style={ROOT} preset="fixed">
-        <CircleLogo
-          viewStyle={{ left: "30%", top: circleHeightInPercentage }}
-          outerCircleColor={palette.white}
-          innerCircleColor={palette.purpleBlue}
-          dimension={150}
-        />
+        <View style={{ height: "100%" }}>
+          <CircleLogo
+            viewStyle={{ left: "30%", top: circleHeight }}
+            outerCircleColor={palette.white}
+            innerCircleColor={palette.purpleBlue}
+            dimension={150}
+          />
+          <GoogleSigninButton
+            style={{ width: 230, height: 48, left: "20%", top: buttonHeight }}
+            size={GoogleSigninButton.Size.Wide}
+            color={GoogleSigninButton.Color.Light}
+            onPress={this.signIn}
+            disabled={this.state.isSigninInProgress}
+          />
+        </View>
       </Screen>
     )
+  }
+
+  private signIn = async () => {
+    try {
+      this.setState({ isSigninInProgress: true })
+      await GoogleSignin.hasPlayServices()
+      const userInfo = await GoogleSignin.signIn()
+      const tokens = await GoogleSignin.getTokens()
+      const circleUser: CircleUserSnapshot = {
+        name: userInfo.user.name,
+        photo: userInfo.user.photo,
+      }
+      this.store.setCircleUser(circleUser, tokens.accessToken)
+    } catch (error) {
+      console.log(error)
+      switch (error.code) {
+        case statusCodes.PLAY_SERVICES_NOT_AVAILABLE:
+          // If google service is not available, whatever
+          break
+        case statusCodes.SIGN_IN_CANCELLED:
+          // User cancelled login, nothing to do
+          break
+        case statusCodes.SIGN_IN_REQUIRED:
+          // Called getTokens wtihout signing in
+          break
+      }
+    } finally {
+      this.setState({ isSigninInProgress: false })
+    }
   }
 }
